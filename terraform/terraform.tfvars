8b16e822cdd3bfcd7ab7ea96adeda96838d8702e
fadzi <kingfadzi@gmail.com>
2024-03-16 22:35:50 -0400

    tf scripts for creatung vms

---
+++ terraform/terraform.tfvars
@@ -0,0 +1,5 @@
+vm_hostname    = "sedna"
+vm_domain      = "local"
+vm_ipv4_address = "192.168.1.158"
+vm_ipv4_netmask = 24
+vm_ipv4_gateway = "192.168.1.1"
---
+++ terraform/variables.tf
@@ -0,0 +1,28 @@
+variable "vm_hostname" {
+  description = "The hostname of the VM"
+  type        = string
+}
+
+variable "vm_domain" {
+  description = "The domain of the VM"
+  type        = string
+  default     = "localdomain"
+}
+
+variable "vm_ipv4_address" {
+  description = "Static IPv4 address for the VM"
+  type        = string
+  default     = ""
+}
+
+variable "vm_ipv4_netmask" {
+  description = "Netmask for the VM's static IPv4 address"
+  type        = number
+  default     = 24
+}
+
+variable "vm_ipv4_gateway" {
+  description = "Default gateway for the VM"
+  type        = string
+  default     = ""
+}
