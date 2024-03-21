323d6747db04d6dd1a3d43d8b6f2e49a51ab4f92
kingfadzi <kingfadzi@gmail.com>
2024-03-21 16:02:46 -0400

    Update setup_vm.pl

--- setup_vm.pl
+++ setup_vm.pl
@@ -3,16 +3,16 @@
 use strict;
 use warnings;
 
-# Prompting for hostname and IP address
+# Prompting for hostname and IP address within the 192.168.1.0/24 network at the start
 print "Please enter the new hostname: ";
 my $hostname = <STDIN>;
 chomp $hostname;
 
-print "Enter IP address (e.g., 192.168.1.2/24): ";
+print "Enter IP address within the 192.168.1.0/24 range (e.g., 192.168.1.2/24): ";
 my $ip_address = <STDIN>;
 chomp $ip_address;
 
-# Check and install the Net::IP Perl module if needed
+# Ensure the Net::IP Perl module is installed for potential future IP calculations
 eval {
     require Net::IP;
     Net::IP->import();
@@ -53,6 +53,10 @@ system("sudo resize2fs /dev/sda3");
 
 # Installing Webmin, overwrite existing GPG key without asking for user confirmation
 print "Installing Webmin...\n";
+if (-e "/usr/share/keyrings/webmin.gpg") {
+    print "Existing Webmin GPG key found. Overwriting...\n";
+    unlink "/usr/share/keyrings/webmin.gpg" or die "Failed to remove existing Webmin GPG key: $!";
+}
 system("curl -fsSL https://download.webmin.com/jcameron-key.asc | sudo gpg --dearmor -o /usr/share/keyrings/webmin.gpg");
 system("echo 'deb [signed-by=/usr/share/keyrings/webmin.gpg arch=amd64] http://download.webmin.com/download/repository sarge contrib' | sudo tee /etc/apt/sources.list.d/webmin.list");
 system("sudo apt-get update");
@@ -69,11 +73,11 @@ close $fh;
 # Setting the hostname
 system("echo $hostname | sudo tee /etc/hostname");
 
-# Hard-code network settings
+# Hard-code network settings for Gateway and DNS to 192.168.1.1
 my $gateway = "192.168.1.1";
 my $dns_servers = "192.168.1.1";
 
-# Automatically configuring the connection for ens160 without activating it
+# Automatically configuring the network connection for ens160
 my $connection_name = `nmcli -g GENERAL.CONNECTION device show ens160`;
 chomp $connection_name;
 if (!$connection_name || $connection_name eq '') {
@@ -83,10 +87,18 @@ if (!$connection_name || $connection_name eq '') {
 }
 system("nmcli con mod \"$connection_name\" ipv4.addresses $ip_address ipv4.gateway $gateway ipv4.dns \"$dns_servers\" ipv4.method manual autoconnect yes");
 
-print "Network settings updated. A reboot is required to apply these changes.\n";
-print "Would you like to reboot now? (y/N): ";
-my $reboot = <STDIN>;
-chomp $reboot;
-if (lc($reboot) eq 'y') {
+print "Network settings updated for $connection_name. Please note: the new network settings will be applied after a reboot.\n";
+
+# Updating /etc/hostname with the provided hostname
+system("echo $hostname | sudo tee /etc/hostname");
+
+# Prompting the user for a system reboot to apply network changes
+print "A reboot is required to apply the network changes. Would you like to reboot now? (y/N): ";
+my $reboot_confirmation = <STDIN>;
+chomp $reboot_confirmation;
+if (lc($reboot_confirmation) eq 'y') {
+    print "Rebooting now...\n";
     system("sudo reboot");
+} else {
+    print "Please remember to reboot the system later to apply the network changes.\n";
 }
